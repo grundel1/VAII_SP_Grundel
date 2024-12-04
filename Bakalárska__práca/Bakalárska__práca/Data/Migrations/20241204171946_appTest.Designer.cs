ba6c27a121847620f47e4cc1eb46cbd39d7c01f7
Samuel Gründel <grundel@stud.uniza.sk>
2024-12-04 18:43:32 +0100

    Update CRUD operácií

--- Bakalárska__práca/Bakalárska__práca/Data/Migrations/20241204171946_appTest.Designer.cs
+++ Bakalárska__práca/Bakalárska__práca/Data/Migrations/20241204171946_appTest.Designer.cs
@@ -4,6 +4,7 @@ using Bakalárska__práca.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Bakalárska__práca.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241204171946_appTest")]
+    partial class appTest
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
